08d8995a6ae5a840876bd406e2191ee04e9e0bf5
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-01 14:49:44 +0100

    Install React-Router and Create Navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as Crown} from '../../assets/crown.svg'
+
+//Styles
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return(
+        <>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <Crown />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </>
+    )
+}
+
+export default Navigation;
